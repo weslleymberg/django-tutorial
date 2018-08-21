1a1560d04e2a7db85891b99e61c2ff966fceb76f
weslleymberg <weslleym.lisboa@gmail.com>
2018-08-21 14:22:45 -0300

    Formatting

--- polls/models.py
+++ polls/models.py
@@ -3,9 +3,12 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
     def __str__(self):
         return self.question_text
 
--- polls/views.py
+++ polls/views.py
@@ -4,17 +4,21 @@ from django.http import HttpResponse
 from .models import Question
 
 # Create your views here.
+
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    return render(request, 'polls/index.html', {'latest_question_list':latest_question_list})
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
+
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question})
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
     return HttpResponse("You are looking at the result of question %s" % question_id)
+
 
 def vote(request, question_id):
     return HttpResponse("You are voting on question %s" % question_id)
